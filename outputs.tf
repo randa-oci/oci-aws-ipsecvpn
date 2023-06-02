f08b594ee94b957be77d3cfe55722d799b9ef9f8
randa-oci <135103097+randa-oci@users.noreply.github.com>
2023-06-02 14:46:33 +0200

    outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,3 @@
 output "Next_steps" {
 value = "This module completes the creation of the new AWS Customer Gateway. At this point you must connect to the AWS Portal and modify the Site to Site VPN selecting the New IaC CGW"
 }
-
-
-
